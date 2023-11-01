204934e994ea3f90d5e9bfd55fc424e41e658fcc
Harshit Singh Chandel <67961339+chandelharshit@users.noreply.github.com>
2023-11-02 03:17:01 +0530

    Added CSS to react Component

---
+++ src/components/AddCSS.js
@@ -0,0 +1,14 @@
+import '../CSS/add.css';
+export default function AddCSS() {
+  return (
+    <div>
+      <h1 className="heading">Harshit Singh Chandel</h1>
+      <p className="para">
+        JavaScript, often abbreviated as JS, is a programming language that is
+        one of the core technologies of the World Wide Web, alongside HTML and
+        CSS. As of 2023, 98.7% of websites use JavaScript on the client side for
+        webpage behavior, often incorporating third-party libraries.
+      </p>
+    </div>
+  );
+}
